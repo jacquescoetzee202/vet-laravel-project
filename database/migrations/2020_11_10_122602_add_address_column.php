1a939e4d56b7155613a4c7c4fd0af432e5e6f86d
Jacques <jacquescoetzee91@gmail.com>
2020-11-10 14:17:05 +0000

    03 migrations challange complete

---
+++ database/migrations/2020_11_10_122602_add_address_column.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAddressColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table("owners", function (Blueprint $table) { 
+            $table->string("address_1",255);
+            $table->string("address_2",255)->nullable();
+            $table->string("town",100);
+            $table->string("postcode",100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table("owners", function (Blueprint $table) { 
+            $table->dropColumn("address_1");
+            $table->dropColumn("address_2");
+            $table->dropColumn("town");
+            $table->dropColumn("postcode");
+        });
+    }
+}
