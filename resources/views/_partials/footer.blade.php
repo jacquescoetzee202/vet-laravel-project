83bc4ac1c1f5b3a960626185f6ea9927e504dccb
Jacques <jacquescoetzee91@gmail.com>
2020-11-12 16:33:28 +0000

    added animal emojis to footer :)

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -1,3 +1,8 @@
 <footer class="mt-4">
-    <p>&copy;Fake 2020</p>
+    <p>&copy;FakeVet 2020</p>
+    <span>&#128018</span>
+    <span>&#129421</span>
+    <span>&#128054</span>
+    <span>&#128008</span>
+    <span>&#129427</span>
 </footer>
