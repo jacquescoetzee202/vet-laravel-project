26b267078750af9805e980ee2564cef8ac843a68
Jacques <jacquescoetzee91@gmail.com>
2020-11-12 16:12:48 +0000

    added app blade for boiler plate

---
+++ resources/views/app.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>My Amazing Blog</title>
+    <link
+    rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <div class="container">
+        <nav class="mt-4 navbar navbar-light bg-light">
+            <a class="navbar-brand" href="/">My Amazing Blog</a>
+        </nav>
+
+        <main class="mt-4">
+          @yield("content")
+        </main>
+        <footer class="mt-4">
+            <p>&copy;Fake 2020</p>
+        </footer>
+    </div>
+  </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,32 +1,15 @@
-<!doctype html>
-<html class="no-js" lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <title>My Amazing Blog</title>
-    <link
-    rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"
-    />
-  </head>
-  <body>
-    <div class="container">
-        <nav class="mt-4 navbar navbar-light bg-light">
-            <a class="navbar-brand" href="/">My Amazing Blog</a>
-        </nav>
 
-        <main class="mt-4">
-            <div class="list-group">
-            <a href="#" class="list-group-item list-group-item-action">
-                <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">Tea</h5>
-                <small>1 Day Ago</small>
-                </div>
-                <p class="mb-1">These teas are the bees knees</p>
-            </a>
-            </div>
-        </main>
-        <footer class="mt-4">
-            <p>&copy;Fake 2020</p>
-        </footer>
-    </div>
-  </body>
-</html>
+@extends("app")
+
+@section("content")
+<div class="list-group">
+    <a href="#" class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">Tea</h5>
+            <small>1 Day Ago</small>
+        </div>
+        <p class="mb-1">These teas are the bees knees</p>
+    </a>
+</div>
+@endsection
+ 
