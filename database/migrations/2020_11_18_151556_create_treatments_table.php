89244fe5d1993750b55c7da4cdcf43d93248990b
Jacques <jacquescoetzee91@gmail.com>
2020-11-18 15:59:44 +0000

    many to many pivot table and treatment table migration

---
+++ database/migrations/2020_11_18_151556_create_treatments_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTreatmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('treatments', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 30);
+        });
+
+        Schema::create('animal_treatment', function (Blueprint $table) {
+            $table->id();
+            
+            // animal_id column with foreign key relationship
+            $table->foreignId("animal_id")
+                ->constrained()
+                ->onDelete("cascade");
+
+            // treatment_id column with foreign key relationship
+            $table->foreignId("treatment_id")
+                ->constrained()
+                ->onDelete("cascade");
+
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('animal_treatment', function (Blueprint $table) {
+            $table->dropForeign(["animal_id"]);
+            $table->dropForeign(["treatment_id"]);
+        });
+        Schema::dropIfExists('treatments');
+        Schema::dropIfExists('animal_treatment');
+    }
+}
