0f4b6ca95fe94cb55a5ca4281a11268a7e6afdcf
Jacques <jacquescoetzee91@gmail.com>
2020-11-18 12:44:59 +0000

    code for review api.php and Animals.php controller

---
+++ app/Http/Controllers/API/Animals.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Owner;
+use App\Models\Animal;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class Animals extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Owner $owner)
+    {
+        return $owner->animals;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Owner $owner, Animal $animal)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    public function show(Owner $owner, Animal $animal)
+    {
+        // show specific animal based on ID
+        return $animal;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\Owners;
-
+use App\Http\Controllers\API\Animals;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,22 +19,42 @@ use App\Http\Controllers\API\Owners;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 Route::group(["prefix" => "owners"], function() {
-    // GET homestead.test/api/owners show all articles
+    // GET homestead.test/api/owners show all owners
     Route::get("",[Owners::class, "index"]);
-    //POST homestead.test/api/owners create an article
+    //POST homestead.test/api/owners create an owner
     Route::post("",[Owners::class, "store"]);
 
     Route::group(["prefix" => "{owner}"], function(){
-        // GET homestead.test/api/owners/1 show the article based off given id
+        // GET homestead.test/api/owners/1 show the owner based off given id
         Route::get("",[Owners::class, "show"]);
-        // PUT homestead.test/api/owners/1 update specific article
+        // PUT homestead.test/api/owners/1 update specific owner
         Route::put("",[Owners::class, "update"]);
-        // DELETE homestead.test/api/owners/1 delete a specific article
+        // DELETE homestead.test/api/owners/1 delete a specific owner
         Route::delete("",[Owners::class, "destroy"]);
+
+        Route::group(["prefix" => "animals"], function(){
+            // GET homestead.test/api/owners/animals show all the animals
+            Route::get("",[Animals::class, "index"]);
+            Route::post("",[Animals::class, "store"]);
+            
+            Route::group(["prefix" => "{animal}"], function(){
+                // GET homestead.test/api/owners/animals/{animal} route that shows a specific animal
+                Route::get("",[Animals::class, "show"]);
+                
+            });
+        });
     });
 
 });
 
 
 
+/*
+
+Add a GET /api/owners/{owner}/animals/{animal} route that shows a specific animal
+Add a POST /api/owners/{owner}/animals route so that you can add an animal to an owner
+Add a PUT /api/owners/{owner}/animals/{animal} route so that you can update an animal
+Add a DELETE /api/owners/{owner}/animals/{animal} route so that you can delete an animal
+*/
